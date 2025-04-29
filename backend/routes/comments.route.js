2b6d0a0e6f8d69bd9d7ae3f5e1e81a87edfb0dc6
Gabriel Richter Reynoso <garey91@gmail.com>
2025-04-29 17:00:39 +0200

    comments post route

---
+++ backend/routes/comments.route.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import Comment from '../models/comments.model.js';
+import BlogPost from '../models/blogpost.model.js';
+import { auth } from '../middleware/auth.js';
+
+const router = express.Router();
+
+router.post('/:id', auth, async (req,res) => {
+    const body = req.body;
+    const postId = req.params.id
+
+    if(!body.comment) {
+        res.status(401).json({message: 'Content needed'})
+    }
+
+    try{
+        const comment = await new Comment({content: body.comment, author: req.user.userId})
+        comment.save();
+
+        await BlogPost.findByIdAndUpdate(postId, { $push: {comments: comment._id}})
+        res.status(201).json({success: true, commentCreated: comment})
+
+
+    } catch(error) {
+        res.status(500).json({message: 'Server error, try again'})
+    }
+ 
+})
+
+export default router;
