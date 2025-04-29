ba19ba09de3e7ae829a35b8a8d812bed500d191e
Gabriel Richter Reynoso <garey91@gmail.com>
2025-04-29 17:01:29 +0200

    styling and import comments component

--- frontend/components/Post.jsx
+++ frontend/components/Post.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import {HandThumbUpIcon, ChatBubbleBottomCenterIcon} from '@heroicons/react/24/solid'
+import {HandThumbUpIcon, ChatBubbleBottomCenterIcon, PaperAirplaneIcon} from '@heroicons/react/24/solid'
+import Comments from './Comments';
 
 const Post = ({post}) => {
     console.log(post);
@@ -8,10 +9,13 @@ const Post = ({post}) => {
             <h4 className='absolute top-0 right-0 text-white'>Posted by: {post.author.username}</h4>
             <h2 className='text-white underline'>{post.title}</h2>
             <p>{post.content}</p>
-            <div className='flex gap-0.5 mt-2'>
+            <div className='flex gap-0.5 mt-2 items-center'>
                 <button className='p-0.5 border-2 rounded-md hover:border-white hover:cursor-pointer'><HandThumbUpIcon className='size-6 text-blue-500'/></button>
+                <p className='text-white'>{post.likes}</p>
                 <button className='p-0.5 border-2 rounded-md hover:border-white hover:cursor-pointer'><ChatBubbleBottomCenterIcon className='size-6 text-blue-500'/></button>
+                <p className='text-white'>{post.comments.length}</p>
             </div>
+            <Comments post={post}/>
         </li>
     )
 }
