0db37a100b133bf4254324d8d8eb631c6e874b86
Gabriel Richter Reynoso <garey91@gmail.com>
2025-04-29 15:15:53 +0200

    add icons

--- frontend/components/Post.jsx
+++ frontend/components/Post.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {HandThumbUpIcon, ChatBubbleBottomCenterIcon} from '@heroicons/react/24/solid'
 
 const Post = ({post}) => {
     console.log(post);
@@ -8,8 +9,8 @@ const Post = ({post}) => {
             <h2 className='text-white underline'>{post.title}</h2>
             <p>{post.content}</p>
             <div className='flex gap-0.5 mt-2'>
-                <button className='p-0.5 border-2 rounded-md hover:border-white hover:cursor-pointer'>Like</button>
-                <button className='p-0.5 border-2 rounded-md hover:border-white hover:cursor-pointer'>Comment</button>
+                <button className='p-0.5 border-2 rounded-md hover:border-white hover:cursor-pointer'><HandThumbUpIcon className='size-6 text-blue-500'/></button>
+                <button className='p-0.5 border-2 rounded-md hover:border-white hover:cursor-pointer'><ChatBubbleBottomCenterIcon className='size-6 text-blue-500'/></button>
             </div>
         </li>
     )
