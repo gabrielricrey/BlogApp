a96e884c94d571a0eb3d27eb5b0d37f8f27d516f
Gabriel Richter Reynoso <garey91@gmail.com>
2025-04-30 14:56:28 +0200

    add signout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,9 +7,18 @@ import RegisterPage from '../pages/RegisterPage';
 import ProfilePage from '../pages/ProfilePage'
 import { MagnifyingGlassIcon, UserCircleIcon, HomeIcon} from '@heroicons/react/24/solid';
 
+
 function App() {
 
   const navigate = useNavigate();
+
+  const signOut = () => {
+    if(localStorage.getItem('token')) {
+      localStorage.removeItem('token');
+      navigate('/')
+
+    }
+  }
 
 
   return (
@@ -20,7 +29,7 @@ function App() {
           <li><button className='flex items-center justify-center' onClick={() => navigate('/start')}><HomeIcon className='size-6 text-white hover:cursor-pointer'/></button></li>
           <li><button className='flex items-center justify-center' onClick={() => navigate('/profile')}><UserCircleIcon className='size-6 text-white hover:cursor-pointer'/></button></li>
           <li><button className='flex items-center justify-center'><MagnifyingGlassIcon className='size-6 text-white hover:cursor-pointer'/></button></li>
-          <li><button className='bg-blue-950 text-white px-2 py-1 rounded-md border-2 border-black hover:cursor-pointer hover:border-white'>Sign Out</button></li>
+          <li><button onClick={signOut} className='bg-blue-950 text-white px-2 py-1 rounded-md border-2 border-black hover:cursor-pointer hover:border-white'>Sign Out</button></li>
           
         </ul>
       </nav>
