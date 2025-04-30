f7060c51a4cd94c701e5d21c695f1ba3934054ea
Gabriel Richter Reynoso <garey91@gmail.com>
2025-04-30 12:49:47 +0200

    add home icon in navbar and navigate to start page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import LoginPage from '../pages/LoginPage';
 import StartPage from '../pages/StartPage';
 import RegisterPage from '../pages/RegisterPage';
 import ProfilePage from '../pages/ProfilePage'
-import { MagnifyingGlassIcon, UserCircleIcon } from '@heroicons/react/24/solid';
+import { MagnifyingGlassIcon, UserCircleIcon, HomeIcon} from '@heroicons/react/24/solid';
 
 function App() {
 
@@ -17,6 +17,7 @@ function App() {
       <nav className="w-[100] bg-blue-900 flex justify-between p-2 items-center">
         <a href="" className='text-white'>BlogApp</a>
         <ul className='flex justify-between items-center gap-2'>
+          <li><button className='flex items-center justify-center' onClick={() => navigate('/start')}><HomeIcon className='size-6 text-white hover:cursor-pointer'/></button></li>
           <li><button className='flex items-center justify-center' onClick={() => navigate('/profile')}><UserCircleIcon className='size-6 text-white hover:cursor-pointer'/></button></li>
           <li><button className='flex items-center justify-center'><MagnifyingGlassIcon className='size-6 text-white hover:cursor-pointer'/></button></li>
           <li><button className='bg-blue-950 text-white px-2 py-1 rounded-md border-2 border-black hover:cursor-pointer hover:border-white'>Sign Out</button></li>
