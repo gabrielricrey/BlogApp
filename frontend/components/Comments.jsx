24b154efa0ab59641696f2aaad152b15dc2c220c
Gabriel Richter Reynoso <garey91@gmail.com>
2025-05-01 13:55:44 +0200

    styling on comment

--- frontend/components/Comments.jsx
+++ frontend/components/Comments.jsx
@@ -5,7 +5,7 @@ import axios from 'axios';
 const Comments = ({ post }) => {
 
     const [comment, setComment] = useState("");
-    console.log(post);
+
     const submitComment = async () => {
         const token = JSON.parse(localStorage.getItem('token'))
 
@@ -25,15 +25,28 @@ const Comments = ({ post }) => {
 
     return (
         <div className='comments mt-2'>
-            <div>
-                <textarea name="" id="" className='bg-white rounded-md' onChange={(e) => setComment(e.target.value)}></textarea>
-                <button onClick={submitComment}><PaperAirplaneIcon className='size-6 text-blue-500' /></button>
+            <div className='w-full flex justify-between gap-2 mb-3'>
+                <textarea name="" id="" className='bg-transparent border-0 border-b-2 resize-none focus:outline-none p-0 leading-tight w-full' onChange={(e) => setComment(e.target.value)}></textarea>
+                <button onClick={submitComment} className='p-0.5 border-2 rounded-md hover:border-white hover:cursor-pointer '><PaperAirplaneIcon className='size-6 text-blue-500' /></button>
             </div>
             <ul className='flex flex-col gap-2'>
                 {post.comments && post.comments.map(comment =>
-                    <li className='rounded-md bg-blue-950 text-white p-0.5 flex justify-between'>
-                        <p>{comment.content}</p>
-                        <p>{comment.author.username}</p>
+                    <li className='rounded-md bg-blue-950 text-white py-1 px-2'>
+                        <div className='flex justify-between'>
+
+                            <p>{comment.content}</p>
+                            <p>{comment.author.username}</p>
+                        </div>
+                        <p className="text-right text-sm text-gray-400">
+                            {new Date(comment.createdAt).toLocaleString('sv-SE', {
+                                year: 'numeric',
+                                month: 'long',
+                                day: 'numeric',
+                                hour: '2-digit',
+                                minute: '2-digit'
+                            })}
+                        </p>
+
                     </li>)}
             </ul>
         </div>
