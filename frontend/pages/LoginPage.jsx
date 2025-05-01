e11b34fc03493356307c75261d3e01dc53af8077
Gabriel Richter Reynoso <garey91@gmail.com>
2025-05-01 13:51:25 +0200

    navigate to start if token exists

--- frontend/pages/LoginPage.jsx
+++ frontend/pages/LoginPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
@@ -9,6 +9,13 @@ const LoginPage = () => {
     let [password,setPassword] = useState("");
 
     let navigate = useNavigate();
+    
+
+    useEffect(() => {
+      if(localStorage.getItem('token')) {
+        navigate('/start')
+      }
+    }, [])
 
     const handeLogin = async () => {
 
