dfb6d8a8b009b0f50eef7355e9664dd377ed3c46
Gabriel Richter Reynoso <garey91@gmail.com>
2025-05-01 13:52:35 +0200

    hide navbar items when not signed in

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -25,6 +25,7 @@ function App() {
     <>
       <nav className="w-[100] bg-blue-900 flex justify-between p-2 items-center">
         <a href="" className='text-white'>BlogApp</a>
+        {localStorage.getItem('token') && 
         <ul className='flex justify-between items-center gap-2'>
           <li><button className='flex items-center justify-center' onClick={() => navigate('/start')}><HomeIcon className='size-6 text-white hover:cursor-pointer'/></button></li>
           <li><button className='flex items-center justify-center' onClick={() => navigate('/profile')}><UserCircleIcon className='size-6 text-white hover:cursor-pointer'/></button></li>
@@ -32,6 +33,7 @@ function App() {
           <li><button onClick={signOut} className='bg-blue-950 text-white px-2 py-1 rounded-md border-2 border-black hover:cursor-pointer hover:border-white'>Sign Out</button></li>
           
         </ul>
+        }
       </nav>
       <Routes>
         <Route path='/' element={<LoginPage/>}/>
