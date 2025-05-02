8368da12623f18d885540f94cc9de820dbc128bc
Gabriel Richter Reynoso <garey91@gmail.com>
2025-05-02 18:57:51 +0200

    profilepage dynamic

--- frontend/pages/ProfilePage.jsx
+++ frontend/pages/ProfilePage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import thumbnail from '../src/assets/thumbnail.png'
 import { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import axios from 'axios'
 
 const ProfilePage = () => {
 
-  const [me, setMe] = useState(null);
+  const [user, setUser] = useState(null);
+
+  const {id} = useParams();
 
   const navigate = useNavigate();
 
@@ -19,11 +21,13 @@ const ProfilePage = () => {
       }
   
       try {
-        const response = await axios.get('http://localhost:3000/api/users/me', {
+
+        const endpoint = id ? `http://localhost:3000/api/users/${id}` : "http://localhost:3000/api/users/me"
+        const response = await axios.get(endpoint, {
           headers: { Authorization: `Bearer ${token}` },
         });
 
-        setMe(response.data)
+        setUser(response.data)
 
       } catch (error) {
         console.log('Token är ogiltig eller har gått ut:', error);
@@ -43,22 +47,22 @@ const ProfilePage = () => {
 
         <div className=' bg-blue-950 p-5 relative flex'>
             <img src={thumbnail} alt="" className='rounded-full size-50' />
-            <h1 className='p-2 rounded-md absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-3xl'>{me && me.username}</h1>
+            <h1 className='p-2 rounded-md absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-3xl'>{user && user.username}</h1>
             <button className='p-2 border-2 bg-green-400 absolute right-5 rounded-md hover:cursor-pointer hover:border-white'>Edit Profile</button>
         </div>
 
         <div className='flex justify-center bg-blue-900 p-5'>
           <div className='flex text-center gap-5'>
             <div className=''>
-              <h4 className='text-white'>{me && me.friends.length}</h4>
+              <h4 className='text-white'>{user && user.friends.length}</h4>
               <h4 className='text-white'>Friends</h4>
             </div>
             <div>
-              <h4 className='text-white'>{me&& me.posts.length}</h4>
+              <h4 className='text-white'>{user&& user.posts.length}</h4>
               <h4 className='text-white'>Posts</h4>
             </div>
             <div>
-              <h4 className='text-white'>{me&& me.comments.length}</h4>
+              <h4 className='text-white'>{user&& user.comments.length}</h4>
               <h4 className='text-white'>Comments</h4>
             </div>
           </div>
