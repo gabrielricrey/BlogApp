6e709e655e71953ad219a00fd4e346be9267897e
Gabriel Richter Reynoso <garey91@gmail.com>
2025-05-02 18:57:35 +0200

    add searchpage path and profileparams path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import LoginPage from '../pages/LoginPage';
 import StartPage from '../pages/StartPage';
 import RegisterPage from '../pages/RegisterPage';
 import ProfilePage from '../pages/ProfilePage'
+import SearchPage from '../pages/SearchPage';
 import { MagnifyingGlassIcon, UserCircleIcon, HomeIcon} from '@heroicons/react/24/solid';
 
 
@@ -29,7 +30,7 @@ function App() {
         <ul className='flex justify-between items-center gap-2'>
           <li><button className='flex items-center justify-center' onClick={() => navigate('/start')}><HomeIcon className='size-6 text-white hover:cursor-pointer'/></button></li>
           <li><button className='flex items-center justify-center' onClick={() => navigate('/profile')}><UserCircleIcon className='size-6 text-white hover:cursor-pointer'/></button></li>
-          <li><button className='flex items-center justify-center'><MagnifyingGlassIcon className='size-6 text-white hover:cursor-pointer'/></button></li>
+          <li><button className='flex items-center justify-center' onClick={() => navigate('/search')}><MagnifyingGlassIcon className='size-6 text-white hover:cursor-pointer'/></button></li>
           <li><button onClick={signOut} className='bg-blue-950 text-white px-2 py-1 rounded-md border-2 border-black hover:cursor-pointer hover:border-white'>Sign Out</button></li>
           
         </ul>
@@ -39,7 +40,9 @@ function App() {
         <Route path='/' element={<LoginPage/>}/>
         <Route path='/start' element={<StartPage/>}/>
         <Route path='/profile' element={<ProfilePage/>}/>
+        <Route path='/profile/:id' element={<ProfilePage/>}/>
         <Route path='/register' element={<RegisterPage/>}/>
+        <Route path='/search' element={<SearchPage/>}/>
       </Routes>
     </>
   )
