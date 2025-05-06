6278ee6e4e89e758e14e3cc00d161a45f32d7be1
Gabriel Richter Reynoso <garey91@gmail.com>
2025-05-06 18:04:40 +0200

    remove verifytoken

--- frontend/pages/StartPage.jsx
+++ frontend/pages/StartPage.jsx
@@ -1,32 +1,21 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios';
 import CreatePost from '../components/CreatePost';
 import Posts from '../components/Posts';
+import {UserContext} from '../context/UserContext'
 
 
 const StartPage = () => {
 
   const navigate = useNavigate();
+  const {loggedInUser} = useContext(UserContext)
 
   useEffect(() => {
-    const verifyToken = async () => {
-      const token = JSON.parse(localStorage.getItem('token'))
-      if (!token) {
-        navigate('/')
-        return;
-      }
-
-      try {
-        await axios.get('http://localhost:3000/auth/verify', { headers: { Authorization: `Bearer ${token}` } })
-      } catch (error) {
-        console.log('Token not valid or expired' + error)
-        localStorage.removeItem('token');
-        navigate('/')
-      }
+    if(!loggedInUser) {
+      navigate('/')
     }
-    verifyToken();
-  }, [])
+  }, [loggedInUser])
 
   return (
     <div className='w-[100] h-screen flex flex-col justify-center items-center'>
