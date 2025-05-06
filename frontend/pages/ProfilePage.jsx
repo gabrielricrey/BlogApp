cdbeb9a144a2547bfe240981645f4cff478666e0
Gabriel Richter Reynoso <garey91@gmail.com>
2025-05-06 18:02:50 +0200

    set user in profilepage to loggedinuser or seached user

--- frontend/pages/ProfilePage.jsx
+++ frontend/pages/ProfilePage.jsx
@@ -1,75 +1,91 @@
-import React from 'react'
-import thumbnail from '../src/assets/thumbnail.png'
-import { useEffect, useState } from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
-import axios from 'axios'
+import React from 'react';
+import thumbnail from '../src/assets/thumbnail.png';
+import { useEffect, useState, useContext } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { UserContext } from '../context/UserContext';
+import axios from 'axios';
 
 const ProfilePage = () => {
-
   const [user, setUser] = useState(null);
 
-  const {id} = useParams();
-
+  const { id } = useParams();
   const navigate = useNavigate();
+  const { loggedInUser } = useContext(UserContext);
 
   useEffect(() => {
     async function getUserInfo() {
-      const token = JSON.parse(localStorage.getItem('token'))
+      const token = JSON.parse(localStorage.getItem('token'));
       if (!token) {
         navigate('/start');
         return;
       }
-  
-      try {
 
-        const endpoint = id ? `http://localhost:3000/api/users/${id}` : "http://localhost:3000/api/users/me"
-        const response = await axios.get(endpoint, {
-          headers: { Authorization: `Bearer ${token}` },
-        });
+      if (id) {
+        try {
+          const endpoint = `http://localhost:3000/api/users/${id}`;
+          const response = await axios.get(endpoint, {
+            headers: { Authorization: `Bearer ${token}` },
+          });
 
-        setUser(response.data)
-
-      } catch (error) {
-        console.log('Token är ogiltig eller har gått ut:', error);
-        navigate('/start');
+          setUser(response.data);
+        } catch (error) {
+          console.log('Token är ogiltig eller har gått ut:', error);
+          navigate('/start');
+        }
       }
     }
-  
-    getUserInfo();
-  }, []);
-  
 
+    getUserInfo();
+  }, [id, navigate]);
+
+  useEffect(() => {
+    if (!id && loggedInUser) {
+      setUser(loggedInUser);
+    }
+  }, [id, loggedInUser]);
+
+  if (!user) return <div className="text-center mt-10 text-white">Laddar profil...</div>;
 
   return (
     <div className='mt-5'>
-
       <div className='border-2 border-black rounded-md m-auto shrink-0 max-w-5xl'>
+        <div className='bg-blue-950 p-5 relative flex'>
+          <img src={thumbnail} alt="" className='rounded-full size-50' />
+          <h1 className='p-2 rounded-md absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-3xl'>
+            {user.username}
+          </h1>
 
-        <div className=' bg-blue-950 p-5 relative flex'>
-            <img src={thumbnail} alt="" className='rounded-full size-50' />
-            <h1 className='p-2 rounded-md absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-3xl'>{user && user.username}</h1>
-            <button className='p-2 border-2 bg-green-400 absolute right-5 rounded-md hover:cursor-pointer hover:border-white'>Edit Profile</button>
+          {user && loggedInUser && user.username === loggedInUser.username && (
+            <button className='p-2 border-2 bg-green-400 absolute right-5 rounded-md hover:cursor-pointer hover:border-white'>
+              Edit Profile
+            </button>
+          )}
+          {user && loggedInUser && user.username !== loggedInUser.username && (
+            <button className='p-2 border-2 bg-blue-300 absolute right-5 rounded-md hover:cursor-pointer hover:border-white'>
+              Add friend
+            </button>
+          )}
         </div>
 
         <div className='flex justify-center bg-blue-900 p-5'>
           <div className='flex text-center gap-5'>
-            <div className=''>
-              <h4 className='text-white'>{user && user.friends.length}</h4>
+            <div>
+              <h4 className='text-white'>{user.friends.length}</h4>
               <h4 className='text-white'>Friends</h4>
             </div>
             <div>
-              <h4 className='text-white'>{user&& user.posts.length}</h4>
+              <h4 className='text-white'>{user.posts.length}</h4>
               <h4 className='text-white'>Posts</h4>
             </div>
             <div>
-              <h4 className='text-white'>{user&& user.comments.length}</h4>
+              <h4 className='text-white'>{user.comments.length}</h4>
               <h4 className='text-white'>Comments</h4>
             </div>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProfilePage
+export default ProfilePage;
