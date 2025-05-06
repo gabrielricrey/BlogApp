6966c1451b5784c965e78241ba29f93ced444064
Gabriel Richter Reynoso <garey91@gmail.com>
2025-05-06 18:03:38 +0200

    navigate to profile when click username

--- frontend/pages/SearchPage.jsx
+++ frontend/pages/SearchPage.jsx
@@ -40,7 +40,7 @@ const SearchPage = () => {
     <div className='flex justify-center flex-col items-center'>
         <input type="text" className='bg-white text-center p-2 outline-0 rounded-md w-md mt-5' onChange={(e) => setSearchInput(e.target.value)}/>
         <ul className='bg-blue-950 w-lg p-3 rounded-md mt-5 border-2'>
-            {result && result.map(user => <li className='text-white'>{user.username}</li>)}
+            {result && result.map(user => <li className='text-white hover:cursor-pointer' onClick={() => navigate(`/profile/${user._id}`)}>{user.username}</li>)}
         </ul>
     </div>
   )
