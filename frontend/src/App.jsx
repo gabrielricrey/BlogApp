414e386e056d21e991018dc64d55e322d0fcb8c7
Gabriel Richter Reynoso <garey91@gmail.com>
2025-05-08 16:15:21 +0200

    shownotifications state and loggedInUser null when signout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useContext } from 'react'
 import './App.css'
 import {Routes,Route, useNavigate} from 'react-router-dom';
 import LoginPage from '../pages/LoginPage';
@@ -6,16 +6,23 @@ import StartPage from '../pages/StartPage';
 import RegisterPage from '../pages/RegisterPage';
 import ProfilePage from '../pages/ProfilePage'
 import SearchPage from '../pages/SearchPage';
-import { MagnifyingGlassIcon, UserCircleIcon, HomeIcon} from '@heroicons/react/24/solid';
+import NotificationContainer from '../components/NotificationContainer';
+import { MagnifyingGlassIcon, UserCircleIcon, HomeIcon, BellIcon} from '@heroicons/react/24/solid';
+import { UserContext } from '../context/UserContext';
 
 
 function App() {
+
+  const [showNotifications, setShowNotifications] = useState(false)
+
+  const {setLoggedInUser} = useContext(UserContext);
 
   const navigate = useNavigate();
 
   const signOut = () => {
     if(localStorage.getItem('token')) {
       localStorage.removeItem('token');
+      setLoggedInUser(null);
       navigate('/')
 
     }
@@ -28,6 +35,7 @@ function App() {
         <a href="" className='text-white'>BlogApp</a>
         {localStorage.getItem('token') && 
         <ul className='flex justify-between items-center gap-2'>
+          <li><button className='flex items-center justify-center'><BellIcon onClick={() => setShowNotifications(prev => !prev)} className='size-6 text-white hover:cursor-pointer'/></button></li>
           <li><button className='flex items-center justify-center' onClick={() => navigate('/start')}><HomeIcon className='size-6 text-white hover:cursor-pointer'/></button></li>
           <li><button className='flex items-center justify-center' onClick={() => navigate('/profile')}><UserCircleIcon className='size-6 text-white hover:cursor-pointer'/></button></li>
           <li><button className='flex items-center justify-center' onClick={() => navigate('/search')}><MagnifyingGlassIcon className='size-6 text-white hover:cursor-pointer'/></button></li>
@@ -36,6 +44,7 @@ function App() {
         </ul>
         }
       </nav>
+      <NotificationContainer show={showNotifications}/>
       <Routes>
         <Route path='/' element={<LoginPage/>}/>
         <Route path='/start' element={<StartPage/>}/>
