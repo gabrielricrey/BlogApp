d08db881c59627a88883453448c87d4227a42aea
Gabriel Richter Reynoso <garey91@gmail.com>
2025-05-08 16:19:25 +0200

    cancel request and remove friend functions

--- frontend/components/FriendOptions.jsx
+++ frontend/components/FriendOptions.jsx
@@ -1,39 +1,98 @@
-import {React, useState, useEffect, useContext} from 'react'
-import { useNavigate , useParams} from 'react-router-dom'
+import { React, useState, useEffect, useContext } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
 import { UserContext } from '../context/UserContext';
 import axios from 'axios';
 
 
 const FriendOptions = () => {
 
-    const [friends,setFriends] = useState(false);
+    const [friends, setFriends] = useState(false);
+    const [pendingRequest,setPendingRequest] = useState(false);
 
-    const {loggedInUser} = useContext(UserContext);
-    const {id} = useParams();
+    const { loggedInUser } = useContext(UserContext);
+    const { id } = useParams();
+
+    useEffect(() => {
+        if (loggedInUser.friends.includes(id)) setFriends(true);
+        if (loggedInUser.sentRequests.includes(id)) setPendingRequest(true);
+    }, [])
 
     const addFriend = async () => {
         const token = JSON.parse(localStorage.getItem('token'));
         if (!token) {
-          navigate('/start');
-          return;
+            navigate('/start');
+            return;
         }
 
-        try{
+        try {
             console.log(id);
             console.log(token);
-            const response = await axios.post(`http://localhost:3000/friends/${id}/send-request/`, {},{headers: { Authorization: `Bearer ${token}`}})
+            const response = await axios.post(`http://localhost:3000/friends/${id}/send-request/`, {}, { headers: { Authorization: `Bearer ${token}` } })
             console.log(response)
-        } catch(error) {
+        } catch (error) {
             console.log(error)
         }
-            
+
+    }
+
+    const removeFriend = async () => {
+        const token = JSON.parse(localStorage.getItem('token'));
+        if (!token) {
+            navigate('/start');
+            return;
+        }
+
+        try {
+            console.log(id);
+            console.log(token);
+            const response = await axios.delete(`http://localhost:3000/friends/${id}/remove-friend/`, { headers: { Authorization: `Bearer ${token}` } })
+            console.log(response)
+        } catch (error) {
+            console.log(error)
+        }
+
+    }
+
+    const cancelRequest = async () => {
+        const token = JSON.parse(localStorage.getItem('token'));
+        if (!token) {
+            navigate('/start');
+            return;
+        }
+
+        try {
+            console.log(id);
+            console.log(token);
+            const response = await axios.delete(`http://localhost:3000/friends/${id}/cancel-request/`, { headers: { Authorization: `Bearer ${token}` } })
+            console.log(response)
+        } catch (error) {
+            console.log(error)
+        }
+
     }
 
 
     return (
-        <button className='p-2 border-2 bg-blue-300 absolute right-5 rounded-md hover:cursor-pointer hover:border-white' onClick={addFriend}>
-            Add friend
-        </button>
+        <>
+            {!friends && !pendingRequest &&
+
+                <button className='p-2 border-2 bg-blue-300 absolute right-5 rounded-md hover:cursor-pointer hover:border-white' onClick={addFriend}>
+                    Add friend
+                </button>
+            }
+            {friends &&
+
+                <button className='p-2 border-2 bg-red-300 absolute right-5 rounded-md hover:cursor-pointer hover:border-white' onClick={removeFriend}>
+                    Remove friend
+                </button>
+            }
+            {pendingRequest &&
+
+                <button className='p-2 border-2 bg-red-300 absolute right-5 rounded-md hover:cursor-pointer hover:border-white' onClick={cancelRequest}>
+                    Cancel request
+                </button>
+            }
+        </>
     )
 }
 
