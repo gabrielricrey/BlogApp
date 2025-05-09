594b8be78714191d81bcc7b6b81c399f1f055e78
Gabriel Richter Reynoso <garey91@gmail.com>
2025-05-09 22:19:21 +0200

    import Plusicon

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ import RegisterPage from '../pages/RegisterPage';
 import ProfilePage from '../pages/ProfilePage'
 import SearchPage from '../pages/SearchPage';
 import NotificationContainer from '../components/NotificationContainer';
-import { MagnifyingGlassIcon, UserCircleIcon, HomeIcon, BellIcon} from '@heroicons/react/24/solid';
+import { MagnifyingGlassIcon, UserCircleIcon, HomeIcon, BellIcon, PlusIcon} from '@heroicons/react/24/solid';
 import { UserContext } from '../context/UserContext';
 
 
@@ -35,6 +35,7 @@ function App() {
         <a href="" className='text-white'>BlogApp</a>
         {localStorage.getItem('token') && 
         <ul className='flex justify-between items-center gap-2'>
+          <li><button className='flex items-center justify-center bg-green-600 px-2 rounded-md py-1 border-2 border-green-800 hover:cursor-pointer hover:border-white'>New Post<PlusIcon onClick={() => setShowNotifications(prev => !prev)} className='size-6 text-white bg-green-600 hover:cursor-pointer'/></button></li>
           <li><button className='flex items-center justify-center'><BellIcon onClick={() => setShowNotifications(prev => !prev)} className='size-6 text-white hover:cursor-pointer'/></button></li>
           <li><button className='flex items-center justify-center' onClick={() => navigate('/start')}><HomeIcon className='size-6 text-white hover:cursor-pointer'/></button></li>
           <li><button className='flex items-center justify-center' onClick={() => navigate('/profile')}><UserCircleIcon className='size-6 text-white hover:cursor-pointer'/></button></li>
