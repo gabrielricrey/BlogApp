e0af6b0bfc96a2c768c232c5e69ff18efa486fb8
Gabriel Richter Reynoso <garey91@gmail.com>
2025-05-09 21:50:40 +0200

    add send token when get friendsposts

--- frontend/context/PostsContext.jsx
+++ frontend/context/PostsContext.jsx
@@ -11,28 +11,32 @@ export const PostsProvider = ({ children }) => {
   const [loading, setLoading] = useState(true);
 
   async function addPost(newPost) {
-        const token = JSON.parse(localStorage.getItem('token'))
+    const token = JSON.parse(localStorage.getItem('token'))
 
-        if (!token) {
-            console.log('No token found')
-            return
-        }
+    if (!token) {
+      console.log('No token found')
+      return
+    }
 
-        try {
-            const response = await axios.post('http://localhost:3000/api/blogpost/', { ...newPost }, { headers: { Authorization: `Bearer ${token}` } })
-            console.log(response.data)
+    try {
+      const response = await axios.post('http://localhost:3000/api/blogpost/', { ...newPost }, { headers: { Authorization: `Bearer ${token}` } })
+      console.log(response.data)
 
-            setPosts((prevPosts) => [...prevPosts, response.data.post])
-        } catch (error) {
-            console.error("Error creating post:", error);
-        }
-    
+      setPosts((prevPosts) => [...prevPosts, response.data.post])
+    } catch (error) {
+      console.error("Error creating post:", error);
+    }
+
   }
 
   useEffect(() => {
     const fetchPosts = async () => {
+      if (!localStorage.getItem('token')) return;
+
+      const token = JSON.parse(localStorage.getItem('token'));
+
       try {
-        const response = await axios.get("http://localhost:3000/api/blogpost/");
+        const response = await axios.get("http://localhost:3000/api/blogpost/byfriends", { headers: { Authorization: `Bearer ${token}` } });
         setPosts(response.data);
       } catch (error) {
         console.error("Error fetching posts:", error);
