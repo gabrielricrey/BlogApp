2738de4ae9e8a6a908a4037150217795a3c32258
Gabriel Richter Reynoso <garey91@gmail.com>
2025-05-10 23:53:44 +0200

    change width on posts

--- frontend/components/Posts.jsx
+++ frontend/components/Posts.jsx
@@ -11,7 +11,7 @@ const Posts = () => {
     
 
   return (
-      <ul className='flex flex-col gap-1 w-full'>
+      <ul className='flex flex-col gap-1 w-3xl'>
         {posts && posts
         .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
         .map((post) => (
