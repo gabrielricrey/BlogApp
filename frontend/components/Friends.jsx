26cf0ff0494a281060b37826aa76e58052adf3d9
Gabriel Richter Reynoso <garey91@gmail.com>
2025-05-10 23:00:07 +0200

    if id in params, fetch friends friends

--- frontend/components/Friends.jsx
+++ frontend/components/Friends.jsx
@@ -1,5 +1,5 @@
 import {React, useState, useEffect} from 'react'
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link, useParams } from 'react-router-dom';
 import axios from 'axios';
 
 const Friends = () => {
@@ -7,6 +7,7 @@ const Friends = () => {
 const [friends,setFriends] = useState(null);
 
 const navigate = useNavigate();
+const {id} = useParams();
 
 useEffect(() => {
 
@@ -18,9 +19,14 @@ useEffect(() => {
         }
         
         const token = JSON.parse(localStorage.getItem('token'));
+        let endpoint = "http://localhost:3000/friends";
+
+        if(id)
+
+            endpoint = `http://localhost:3000/friends/user/${id}`
         
         try {
-            const response = await axios.get('http://localhost:3000/friends',{headers: {Authorization: `Bearer ${token}`}})
+            const response = await axios.get(endpoint,{headers: {Authorization: `Bearer ${token}`}})
             console.log(response);
             setFriends(response.data)
         } catch (error) {
@@ -32,11 +38,11 @@ useEffect(() => {
     getFriends();
 
 
-},[])
+},[id])
 
   return (
     <>
-        {friends && friends.map((friend,i) => <li key={i}>{friend.username}</li>)}
+        {friends && friends.map((friend,i) => <li key={i} className='w-full p-5 bg-blue-900 text-white'><Link to={`/profile/${friend._id}`}>{friend.username}</Link></li>)}
     </>
   )
 }
