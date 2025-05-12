f5cc71c72a3f09db12a232d6da2456f9f630b67e
Gabriel Richter Reynoso <garey91@gmail.com>
2025-05-12 14:34:40 +0200

    like model

---
+++ backend/models/like.model.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+
+const likeSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+})
+
+const Like = mongoose.model('Like', likeSchema);
+
+export default Like;
