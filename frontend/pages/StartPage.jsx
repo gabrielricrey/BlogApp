4253a8e307768206e8d3b4dfc1d10559541180e7
Gabriel Richter Reynoso <garey91@gmail.com>
2025-05-12 14:20:35 +0200

    fetchPosts

--- frontend/pages/StartPage.jsx
+++ frontend/pages/StartPage.jsx
@@ -4,6 +4,7 @@ import axios from 'axios';
 import CreatePost from '../components/CreatePost';
 import Posts from '../components/Posts';
 import {UserContext} from '../context/UserContext'
+import { PostsContext } from '../context/PostsContext';
 
 
 const StartPage = () => {
@@ -11,10 +12,14 @@ const StartPage = () => {
   const navigate = useNavigate();
   const {loggedInUser} = useContext(UserContext)
 
+  const {fetchPosts} = useContext(PostsContext);
+
   useEffect(() => {
     if(!localStorage.getItem('token')) {
       navigate('/')
     }
+
+    fetchPosts();
   }, [])
 
   return (
