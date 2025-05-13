25dc2ef294c7a1eec5822c1f99b511817477063d
Gabriel Richter Reynoso <garey91@gmail.com>
2025-05-13 14:57:50 +0200

    add header

--- frontend/pages/CreatePostPage.jsx
+++ frontend/pages/CreatePostPage.jsx
@@ -1,11 +1,18 @@
-import {React} from 'react'
+import { React } from 'react'
 import CreatePost from '../components/CreatePost';
 
 const CreatePostPage = () => {
 
 
     return (
-        <CreatePost/>
+        <div className='w-full sm:w-[calc(100%-250px)] sm:ml-[250px]'>
+            <div className='h-[70px] border-b-1 flex items-center'>
+                <h1 className='text-white ml-6'>Create Post</h1>
+            </div>
+
+
+            <CreatePost />
+        </div>
     )
 }
 
