25e6951842b5150de552e4977e7d881213615787
Gabriel Richter Reynoso <garey91@gmail.com>
2025-05-14 14:18:23 +0200

    import Comment

--- backend/routes/blogpost.route.js
+++ backend/routes/blogpost.route.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import BlogPost from '../models/blogpost.model.js';
 import User from '../models/user.model.js';
+import Comment from '../models/comments.model.js';
 import { auth } from '../middleware/auth.js';
 
 const router = express.Router();
