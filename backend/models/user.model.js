34647e8517583e0898cde2df6861f942301e5a0b
Gabriel Richter Reynoso <garey91@gmail.com>
2025-05-19 16:31:35 +0200

    change match to validate

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -2,56 +2,61 @@ import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
 
 const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    match: [/^[a-zA-Z0-9_]{3,30}$/, 'Användarnamnet får bara innehålla bokstäver, siffror och understreck.']
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 8,
-    match: [
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-      'Lösenordet måste innehålla minst 8 tecken, inklusive en stor bokstav, en liten bokstav, en siffra och ett specialtecken.'
-    ]
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    match: [/^\S+@\S+\.\S+$/, 'Ogiltig e-postadress.']
-  },
-  friends: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
-  }],
-  friendRequests: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
-  }],
-  sentRequests: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
-  }],
-  posts: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'BlogPost'
-  }],
-  comments: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Comment'
-  }]
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/^[a-zA-Z0-9_]{3,30}$/, 'Användarnamnet får bara innehålla bokstäver, siffror och understreck.']
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8,
+        validate: {
+            validator: function (value) {
+                if (this.isModified('password')) {
+                    return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(value);
+                }
+                return true;
+            },
+            message: 'Lösenordet måste innehålla minst 8 tecken, inklusive en stor bokstav, en liten bokstav, en siffra och ett specialtecken.'
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/^\S+@\S+\.\S+$/, 'Ogiltig e-postadress.']
+    },
+    friends: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    friendRequests: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    sentRequests: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    posts: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'BlogPost'
+    }],
+    comments: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Comment'
+    }]
 })
 
-userSchema.pre("save", async function(next) {
-  if (!this.isModified("password")) {
-    return next();
-  }
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        return next();
+    }
 
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
 });
 
 export default mongoose.model('User', userSchema);
