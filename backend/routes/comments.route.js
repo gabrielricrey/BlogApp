f42a1faa9a4492b91ae89a34cc47d5061ffd412a
Gabriel Reynoso <garey91@gmail.com>
2025-05-28 13:18:25 +0200

    refactor: improve comments component with options for user actions and enhance comment submission logic

--- backend/routes/comments.route.js
+++ backend/routes/comments.route.js
@@ -3,30 +3,61 @@ import Comment from '../models/comments.model.js';
 import BlogPost from '../models/blogpost.model.js';
 import User from '../models/user.model.js'
 import { auth } from '../middleware/auth.js';
+import mongoose from 'mongoose';
 
 const router = express.Router();
 
-router.post('/:id', auth, async (req,res) => {
+router.post('/:id', auth, async (req, res) => {
     const body = req.body;
     const postId = req.params.id
 
-    if(!body.comment) {
-        res.status(401).json({message: 'Content needed'})
+    if (!body.comment) {
+        res.status(401).json({ message: 'Content needed' })
     }
 
-    try{
-        const comment = await new Comment({content: body.comment, author: req.user.userId, post: postId})
+    try {
+        const comment = await new Comment({ content: body.comment, author: req.user.userId, post: postId })
         comment.save();
 
-        await BlogPost.findByIdAndUpdate(postId, { $push: {comments: comment._id}})
-        await User.findByIdAndUpdate(req.user.userId, { $push: {comments: comment._id}})
-        res.status(201).json({success: true, commentCreated: comment})
+        await BlogPost.findByIdAndUpdate(postId, { $push: { comments: comment._id } })
+        await User.findByIdAndUpdate(req.user.userId, { $push: { comments: comment._id } })
+        res.status(201).json({ success: true, commentCreated: comment })
 
 
-    } catch(error) {
-        res.status(500).json({message: 'Server error, try again'})
+    } catch (error) {
+        res.status(500).json({ message: 'Server error, try again' })
     }
- 
+
+})
+
+router.delete('/:id', auth, async (req, res) => {
+    const { id } = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).json({ message: 'Invalid comment ID' });
+    }
+
+    try {
+        const comment = await Comment.findById(id);
+        if (!comment) {
+            return res.status(404).json({ message: 'Comment not found' });
+        }
+
+        if (comment.author.toString() !== req.user.userId) {
+            return res.status(403).json({ message: 'You are not authorized to delete this comment' });
+        }
+
+        await Comment.findByIdAndDelete(id);
+        await BlogPost.findByIdAndUpdate(comment.post, { $pull: { comments: id } });
+        await User.findByIdAndUpdate(req.user.userId, { $pull: { comments: id } });
+
+        res.status(200).json({ success: true, message: 'Comment deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ message: 'Server error, try again' });
+        console.error('Error deleting comment:', error);
+    }
+
+
 })
 
 export default router;
--- frontend/components/Comments.jsx
+++ frontend/components/Comments.jsx
@@ -1,56 +1,90 @@
-import React, { useState } from 'react'
-import { PaperAirplaneIcon } from '@heroicons/react/24/solid'
-import axios from 'axios';
+import React, { useState, useContext } from "react";
+import {
+  PaperAirplaneIcon,
+  EllipsisHorizontalIcon,
+} from "@heroicons/react/24/solid";
+import axios from "axios";
+import PostAndCommentOptions from "./PostAndCommentOptions";
+import { UserContext } from "../context/UserContext";
 
 const Comments = ({ post }) => {
+  const [comment, setComment] = useState("");
+  const [showOptions, setShowOptions] = useState(false);
 
-    const [comment, setComment] = useState("");
+  const { loggedInUser } = useContext(UserContext);
+  const submitComment = async () => {
+    const token = JSON.parse(localStorage.getItem("token"));
 
-    const submitComment = async () => {
-        const token = JSON.parse(localStorage.getItem('token'))
-
-        if (!token) {
-            console.log('No token found')
-            return
-        }
-
-        try {
-            const response = await axios.post(`http://localhost:3000/comments/${post._id}`, { comment }, { headers: { Authorization: `Bearer ${token}` } })
-            console.log(response.data)
-        } catch (error) {
-            console.error("Error creating comment:", error);
-        }
-
+    if (!token) {
+      console.log("No token found");
+      return;
     }
 
-    return (
-        <div className='comments mt-2'>
-            <div className='w-full flex justify-between gap-2 mb-3'>
-                <textarea name="" id="" className='bg-transparent border-0 border-b-2 resize-none focus:outline-none p-0 leading-tight w-full' onChange={(e) => setComment(e.target.value)}></textarea>
-                <button onClick={submitComment} className='p-0.5 border-2 rounded-md hover:border-white hover:cursor-pointer '><PaperAirplaneIcon className='size-6 text-blue-500' /></button>
-            </div>
-            <ul className='flex flex-col gap-2'>
-                {post.comments && post.comments.map(comment =>
-                    <li className='rounded-md bg-blue-950 text-white py-1 px-2'>
-                        <div className='flex justify-between'>
+    try {
+      const response = await axios.post(
+        `http://localhost:3000/comments/${post._id}`,
+        { comment },
+        { headers: { Authorization: `Bearer ${token}` } }
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.error("Error creating comment:", error);
+    }
+  };
 
-                            <p>{comment.content}</p>
-                            <p>{comment.author.username}</p>
-                        </div>
-                        <p className="text-right text-sm text-gray-400">
-                            {new Date(comment.createdAt).toLocaleString('sv-SE', {
-                                year: 'numeric',
-                                month: 'long',
-                                day: 'numeric',
-                                hour: '2-digit',
-                                minute: '2-digit'
-                            })}
-                        </p>
+  return (
+    <div className="comments mt-2">
+      <div className="w-full flex justify-between gap-2 mb-3">
+        <textarea
+          name=""
+          id=""
+          className="bg-transparent border-0 border-b-2 resize-none focus:outline-none p-0 leading-tight w-full"
+          onChange={(e) => setComment(e.target.value)}
+        ></textarea>
+        <button
+          onClick={submitComment}
+          className="p-0.5 border-2 rounded-md hover:border-white hover:cursor-pointer "
+        >
+          <PaperAirplaneIcon className="size-6 text-blue-500" />
+        </button>
+      </div>
+      <ul className="flex flex-col gap-2">
+        {post.comments &&
+          post.comments.map((comment) => (
+            <li className="rounded-md relative bg-blue-950 text-white py-1 px-2">
+              {loggedInUser &&
+                comment.author._id === loggedInUser._id?.toString() && (
+                  <button
+                    onClick={() => setShowOptions(!showOptions)}
+                    className="hover:cursor-pointer"
+                  >
+                    <EllipsisHorizontalIcon className="absolute top-4 right-4 z-10 text-white size-6" />
+                  </button>
+                )}
+              {showOptions && (
+                <PostAndCommentOptions
+                  setShowOptions={setShowOptions}
+                  content={comment}
+                />
+              )}
+              <div className="flex justify-between">
+                <p>{comment.content}</p>
+                <p>{comment.author.username}</p>
+              </div>
+              <p className="text-right text-sm text-gray-400">
+                {new Date(comment.createdAt).toLocaleString("sv-SE", {
+                  year: "numeric",
+                  month: "long",
+                  day: "numeric",
+                  hour: "2-digit",
+                  minute: "2-digit",
+                })}
+              </p>
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+};
 
-                    </li>)}
-            </ul>
-        </div>
-    )
-}
-
-export default Comments
+export default Comments;
