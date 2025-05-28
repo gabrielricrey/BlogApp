3f72489ecc86d917075ab39abcdc9ddc1e98539b
Gabriel Reynoso <garey91@gmail.com>
2025-05-28 13:18:02 +0200

    fix: correct nesting of UserProvider and PostsProvider in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,20 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter as Router } from 'react-router-dom';
-import { PostsProvider } from '../context/PostsContext.jsx'
-import { UserProvider } from '../context/UserContext.jsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter as Router } from "react-router-dom";
+import { PostsProvider } from "../context/PostsContext.jsx";
+import { UserProvider } from "../context/UserContext.jsx";
 
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <UserProvider>
       <Router>
-        <PostsProvider>
-          <App />
-        </PostsProvider>
+        <UserProvider>
+          <PostsProvider>
+            <App />
+          </PostsProvider>
+        </UserProvider>
       </Router>
     </UserProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
